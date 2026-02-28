efb563815fe4403f5096f780c7fdecbbb0411c78
Ibtissem mallek <ibtissemmallek0@gmail.com>
2026-02-28 21:40:01 +0100

    commit ibtissem

--- src/main/java/tn/association/med/MedApplication.java
+++ src/main/java/tn/association/med/MedApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(MedApplication.class, args);
 	}
 
