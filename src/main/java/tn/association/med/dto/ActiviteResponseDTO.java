1297f3b5f7acfb48b4ecb7ddbac0feebbc01a60a
pferepo <pfe2475@gmail.com>
2026-03-08 17:05:55 +0100

    1 . Notification => création activite avec liste de emails 2. AJout query dans UserRepo

--- src/main/java/tn/association/med/dto/ActiviteResponseDTO.java
+++ src/main/java/tn/association/med/dto/ActiviteResponseDTO.java
@@ -2,9 +2,12 @@ package tn.association.med.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import tn.association.med.entities.User;
 import tn.association.med.enums.StatutActivite;
+import tn.association.med.enums.TypeActivite;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -13,8 +16,8 @@ public class ActiviteResponseDTO {
     private Long id;
     private String titre;
     private String description;
-    private String type;
-
+    private TypeActivite type;
+    private List<String> membre;
     private StatutActivite statut;
     private StatutActivite statutProposition;
 
