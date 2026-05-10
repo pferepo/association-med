cb25f41ec501091e62a896e7ba52db87adfaa9d9
pferepo <pfe2475@gmail.com>
2026-05-10 13:50:40 +0100

    - Ajout Heritage entre User et (admin, membre_executif)

--- src/main/java/tn/association/med/entities/User.java
+++ src/main/java/tn/association/med/entities/User.java
@@ -1,62 +1,50 @@
 package tn.association.med.entities;
-
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.DiscriminatorFormula;
 import tn.association.med.enums.Genre;
 import tn.association.med.enums.Role;
-
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "role", discriminatorType = DiscriminatorType.STRING)
-@DiscriminatorFormula("case when role in ('ADMIN') then 'ADMIN' when role in ('MEMBRE_BUREAU_EXECUTIF') then 'MEMBRE_BUREAU_EXECUTIF' else 'USER' end")
-@DiscriminatorValue("USER")
+@DiscriminatorFormula(
+        "case when role in ('ADMIN') then 'ADMIN' " +
+                "when role in ('MEMBRE_BUREAU_EXECUTIF') then 'MEMBRE_BUREAU_EXECUTIF' " +
+                "else 'MEMBRE_INVITE' end"
+)
+@DiscriminatorValue("MEMBRE_INVITE")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nom;
     private String prenom;
-
     @Column(unique = true, nullable = false)
     private String email;
-
     private String password;
-
     private String imageUrl;
-
     @Enumerated(EnumType.STRING)
-    @Column(name = "role", insertable = false, updatable = false)
+    @Column(name = "role")
     private Role role;
-
     @Enumerated(EnumType.STRING)
     private Genre genre;
-
     private Boolean active;
-
     private String tel;
-
     private String grade;
-
     private String cin;
-
     private LocalDateTime createdAt;
-
     @PrePersist
     public void prePersist() {
         this.createdAt = LocalDateTime.now();
         if (this.active == null) {
-            this.active = false; //  utilisateur inactif par défaut
+            this.active = false;
         }
     }
 }
